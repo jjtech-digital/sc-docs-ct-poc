da7aa1cc76f1ca20398833df1a18f7f0aaaa473e
Jia-A <155526414+JiyaAgrawal20@users.noreply.github.com>
2025-05-06 09:16:36 +0530

    fix: :bug: Correct import path for ProductProps and update addToCart function argument

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useCart } from "@/context/CartContext";
-import { ProductProps } from "@/types/ProductProps";
+import { ProductProps } from "@/types/types";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -26,7 +26,7 @@ const ProductCard = ({ product }: { product: ProductProps }) => {
         <h3 className="text-lg">{name?.["en-US"]}</h3>
         <p className="text-gray-700 mt-2 font-semibold">${price.centAmount / 100}</p>
         <button
-          onClick={() => addToCart(product)}
+          onClick={() => addToCart(product.id)}
           className="mt-4 w-full bg-black text-white py-2 rounded border hover:bg-white hover:text-black hover:border hover:border-black transition cursor-pointer font-semibold"
         >
           Add to Cart
