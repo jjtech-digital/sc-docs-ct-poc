379f4502052fb684a6131889a7a1cc95adef47d3
Anmol <anmol@compose.co.in>
2025-07-17 13:07:35 +0530

    Build fix

--- src/app/order-confirmation/page.tsx
+++ src/app/order-confirmation/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import React, { Suspense } from "react";
+import { OrderConfirmation as OrderConfirmationClient } from "../../components/order-confirmation";
 
 const OrderConfirmation = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <OrderConfirmation />
+      <OrderConfirmationClient />
     </Suspense>
   );
 };
