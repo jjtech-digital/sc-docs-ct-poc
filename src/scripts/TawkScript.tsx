ecc730a5e52cb2b705ad6421685aac762cdfad5c
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2025-07-17 20:31:36 +0530

    fix: remove redundant script insertion in TawkScript component

--- src/scripts/TawkScript.tsx
+++ src/scripts/TawkScript.tsx
@@ -13,7 +13,6 @@ s1.async=true;
 s1.src='https://embed.tawk.to/685539d433b4bd190b81f5f8/1iu6ejq53';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
 })();
 
       `}
