fe1a5924f04084e5176b1cb445eb1dd17377cfc2
shilpesaxena <shilpesaxena@compsoe.co.in>
2025-07-23 10:12:26 +0530

    refactor: simplify order confirmation page props and enhance order data structure

--- src/app/order-confirmation/page.tsx
+++ src/app/order-confirmation/page.tsx
@@ -2,18 +2,29 @@ import { apiRoot } from "@/lib/ctClient";
 import { redirect } from "next/navigation";
 import OrderConfirmation from "./order-confirmation";
 
-interface PageProps {
-  searchParams: { orderId?: string };
-}
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ orderId?: string }>;
+}) {
+  const { orderId } = await searchParams;
 
-export default async function Page({ searchParams }: PageProps) {
-  const orderId = searchParams.orderId;
   if (!orderId) redirect("/");
 
   try {
     const res = await apiRoot.orders().withId({ ID: orderId }).get().execute();
-    const order = res.body;
-
+    const order = {
+      ...res.body,
+      lineItems: res.body.lineItems.map(
+        (item: (typeof res.body.lineItems)[number]) => ({
+          ...item,
+          price: {
+            ...item.price,
+            discounted: !!item.price.discounted,
+          },
+        })
+      ),
+    };
     return <OrderConfirmation order={order} />;
   } catch {
     return <div>Order not found.</div>;
